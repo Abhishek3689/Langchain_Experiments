24743f21ac4a8b25758fcb54ed7376957e425708
Abhishek3689 <abhisek.nishad3689@gmail.com>
2025-04-16 15:28:07 +0530

    prompts_added

--- Prompts/prompt_template.py
+++ Prompts/prompt_template.py
@@ -1,7 +1,24 @@
 import os
-from langchain_core.prompts import PromptTemplate
+#from langchain_core.prompts import PromptTemplate
+from langchain.prompts import PromptTemplate
 from dotenv import load_dotenv
 from langchain_huggingface import ChatHuggingFace,HuggingFaceEndpoint
 load_dotenv()
 
-llm=HuggingFaceEndpoint
+llm=HuggingFaceEndpoint(
+    repo_id='mistralai/Mistral-7B-Instruct-v0.3',
+    task='text-generation',
+    temperature=.7
+)
+
+template=PromptTemplate(template='Tell me about {country} in {number} lines',
+                      input_variables=['country'])
+
+country=input("enter the country name")
+number=input("enter the number of lines to demostrate :")
+prompt=template.invoke({'country':country,'number':number})
+
+model=ChatHuggingFace(llm=llm)
+response=model.invoke(prompt)
+
+print(response.content)
